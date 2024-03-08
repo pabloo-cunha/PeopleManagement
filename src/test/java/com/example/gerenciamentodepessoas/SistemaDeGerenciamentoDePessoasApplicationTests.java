d1044c0e0af70b37a4c78bb15774bc34c5768893
pabloo-cunha <pablo.limacunha@outlook.com>
2024-03-07 23:08:53 -0300

    first commit

---
+++ src/test/java/com/example/gerenciamentodepessoas/SistemaDeGerenciamentoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamentodepessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerenciamentoDePessoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
